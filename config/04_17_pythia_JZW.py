85bfd953191f89cbce2f74201285ea57464d1d51
Brian Amadio <btamadio@gmail.com>
2017-04-17 17:30:58 -0700

    pythia JZW config file

---
+++ config/04_17_pythia_JZW.py
@@ -0,0 +1,14 @@
+{
+"date": "04_17",
+"job_name": "pythiaJZW",
+"bkg_file": "data/largeR_trig/pythia_JZW.csv",
+"web_path": "/project/projectdirs/atlas/www/multijet/RPV/btamadio/bkgEstimation/",
+"is_mc": true,
+"mc_label": "Pythia Weighted",
+"n_toys": 100, 
+"inject_sig": false,
+"sig_files": [],
+"sig_mult": 0.0, 
+"sig_dsid": 0, 
+"response_plots": ["4js1VRb9bU"]
+}
