65abdbb6d09f0147ae1b15d7d805e7bd69756cad
Brian Amadio <btamadio@gmail.com>
2017-04-19 08:59:12 -0700

    two config files

---
+++ config/04_18_combined_MC.py
@@ -0,0 +1,14 @@
+{
+"date": "04_18",
+"job_name": "combined_multijet",
+"bkg_file": "data/largeR_trig/combined_MC.csv",
+"web_path": "/project/projectdirs/atlas/www/multijet/RPV/btamadio/bkgEstimation/",
+"is_mc": true,
+"mc_label": "Combined Multijet",
+"n_toys": 60, 
+"inject_sig": false,
+"sig_files": [],
+"sig_mult": 0.0, 
+"sig_dsid": 0, 
+"response_plots": ["4js1VRb9bU"]
+}
---
+++ config/04_18_test.py
@@ -0,0 +1,15 @@
+{
+"date": "04_18",
+"job_name": "test_pythia",
+"bkg_file": "data/largeR_trig/pythia_JZW.csv",
+"web_path": "/project/projectdirs/atlas/www/multijet/RPV/btamadio/bkgEstimation/",
+"is_mc": true,
+"mc_label": "",
+"n_toys": 10,
+"inject_sig": false,
+"sig_files": [],
+"sig_mult": 0.0,
+"sig_dsid": 0,
+"response_plots": [],
+"MJ_plots":["5jSRb1"]
+}
