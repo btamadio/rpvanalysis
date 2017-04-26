9c2910fe3d6b726e5808ae3815326384006230d7
Brian Amadio <btamadio@gmail.com>
2017-04-26 12:00:43 -0700

    bunch of config files

---
+++ config/04_25_Sherpa_HTtrig_template_3.py
@@ -0,0 +1,18 @@
+{
+"date": "04_25",
+"job_name": "sherpa_HTtrig_template_1",
+"bkg_file": "data/HT_trig/sherpa_HTtrig_njet2.csv",
+"web_path": "/project/projectdirs/atlas/www/multijet/RPV/btamadio/bkgEstimation/",
+"is_mc": true,
+"mc_label": "Sherpa",
+"n_toys": 100,
+"inject_sig": false,
+"sig_files": [],
+"sig_mult": 0.0,
+"sig_dsid": 0,
+"template_type":3,
+"MJ_plots":["4jVRb9","4jVRb1","4jSRb9","4jSRb1",
+            "5jVRb9","5jVRb1","5jSRb9","5jSRb1"],
+"response_plots":["3jVR","4j","5j"],
+"blinded":false
+}
