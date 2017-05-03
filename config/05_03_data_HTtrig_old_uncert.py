081170b9cf2b2f6128011dcc8eef55e57fa0621b
Brian Amadio <btamadio@gmail.com>
2017-05-03 16:44:02 -0700

    old uncert

---
+++ config/05_03_data_HTtrig_old_uncert.py
@@ -0,0 +1,22 @@
+{
+"date": "05_03",
+"job_name": "data_HTtrig_old_uncert",
+"bkg_file": "data/HT_trig/data_HT_njet2.csv",
+"web_path": "/project/projectdirs/atlas/www/multijet/RPV/btamadio/bkgEstimation/",
+"hist_path":"/project/projectdirs/atlas/btamadio/RPV_SUSY/PyMJ/hists",
+"is_mc": false,
+"mc_label": "",
+"n_toys": 100,
+"inject_sig": false,
+"sig_files": [],
+"sig_mult": 0.0,
+"sig_dsid": 0,
+"template_type":0,
+"MJ_plots":["4jVRb9","4jVRb1","4jSRb9","4jSRb1",
+            "5jVRb9","5jVRb1","5jSRb9","5jSRb1"],
+"blinded":true,
+"uncertainties":[0.124,0.059,0.059,
+                 0.126,0.064,0.064,
+                 0.112,0.073,0.073,
+                 0.135,0.092,0.092]
+}
